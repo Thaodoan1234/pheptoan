4c52797e59d3e47a2c5aff7c06bd0b99fbd52b43
Thaodoan1234 <thao.doan1@onemount.com>
2021-08-06 01:40:50 +0700

    Initial Commit

---
+++ src/main/java/Pheptoan.java
@@ -0,0 +1,19 @@
+public class Pheptoan {
+    public static void main(String[] args) {
+        float a = 7F;
+        float b = 6F;
+        float tong,hieu,tich,thuong;
+
+        tong = a + b;
+        hieu = a - b;
+        tich = a * b;
+        thuong = a/b;
+
+        System.out.println("Số thứ nhất = " + a);
+        System.out.println("Số thứ hai = " + b);
+        System.out.println("Tổng hai số = " + tong);
+        System.out.println("Hiệu hai số = " + hieu);
+        System.out.println("Tích hai số = " + tich);
+        System.out.println("Thương của hai số = " + thuong);
+    }
+}
